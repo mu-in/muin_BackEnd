8e3c1fb94d2b3ccb35e8d0d1cbd59aeb24f93331
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-09-21 18:21:06 +0900

    chore : 도메인 추가 [#1] - data.sql로 db 초기화 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into tag(keyword) values('SNACK');
+-- insert into user(uuid, email, name, role) values('user111', 'kim@email.com', 'kimmuin', 'USER');
+-- insert into store(latitude, longitude, name, uuid) values(13.777, 210.111, '무인아이스크림', 'store111',1, 1);
