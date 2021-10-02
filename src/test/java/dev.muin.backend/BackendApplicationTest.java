425926cc1d58cfc53f59524cf54eb1d41fc75aab
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-10-03 00:40:55 +0900

    chore : CI오류 해결 - test하위에 별도 application.yml로 오버라이드하여 main 설정파일 로드하여 초기화하는 오류 제거

---
+++ src/test/java/dev.muin.backend/BackendApplicationTest.java
@@ -0,0 +1,12 @@
+package dev.muin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
