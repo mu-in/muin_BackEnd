185b4594d6b3406b868e34a103c5743c691f7779
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-10-10 21:08:45 +0900

    feat : JWT 처리 로직 생성 [#10]
    - JWT인증필터: 특정 url(로그인, 회원가입)요청시엔 무시

--- src/main/java/dev/muin/backend/config/auth/JwtAuthenticationFilter.java
+++ src/main/java/dev/muin/backend/config/auth/JwtAuthenticationFilter.java
@@ -21,15 +21,19 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request,
                          ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
-        // 헤더에서 jwt를 읽어온다.
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        String path = ((HttpServletRequest)request).getRequestURI();
 
-        //유효한 토큰인지 확인
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            // 토큰이 유효하면 토큰에서 유저 정보를 받아온다.
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            // SecurityContext에 Authentication 객체를 저장
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if(!path.equals("/user/login") && !path.equals("/user/join")) {
+            // 헤더에서 jwt를 읽어온다.
+            String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+
+            //유효한 토큰인지 확인
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                // 토큰이 유효하면 토큰에서 유저 정보를 받아온다.
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                // SecurityContext에 Authentication 객체를 저장
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
         chain.doFilter(request, response);
     }
