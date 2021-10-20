5b45eb95e4c4984f5b0835eb431dbd3c46717503
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-10-20 13:31:16 +0900

    feat : uuid base32-encoding revert [#15]

--- src/main/java/dev/muin/backend/service/UserService.java
+++ src/main/java/dev/muin/backend/service/UserService.java
@@ -4,7 +4,6 @@ import dev.muin.backend.config.jwt.JwtTokenProvider;
 import dev.muin.backend.domain.User.Role;
 import dev.muin.backend.domain.User.User;
 import dev.muin.backend.domain.User.UserRepository;
-import dev.muin.backend.util.Base32Util;
 import dev.muin.backend.web.request.LoginRequest;
 import dev.muin.backend.web.response.LoginResponse;
 import dev.muin.backend.web.response.UserResponse;
@@ -29,16 +28,13 @@ public class UserService {
                 .orElse(null);
         if(member == null) member = join(loginRequest);
         String jwt = jwtTokenProvider.createToken(member.getEmail());
-        String encodedUuid = Base32Util.encode(member.getUuid());
-        return new LoginResponse(jwt, encodedUuid, member.getRole());
+        return new LoginResponse(jwt, member.getUuid(), member.getRole());
     }
 
     @Transactional(readOnly = true)
     public UserResponse myInfo(String uuid) {
-        uuid = Base32Util.decode(uuid);
         User member = userRepository.findByUuid(uuid).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자 입니다."));
-        String encodedUuid = Base32Util.encode(member.getUuid());
-        return UserResponse.of(member, encodedUuid);
+        return UserResponse.of(member);
     }
 
     private User join(LoginRequest loginRequest) throws IllegalArgumentException{
