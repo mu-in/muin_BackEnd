1ad80c4acb2a1afb868a33b14f6c6c08e0715ae9
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-10-23 01:11:27 +0900

    reft : 매니저 인증 [#28]
    - 메서드 미사용 파라미터 제거

--- src/main/java/dev/muin/backend/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/dev/muin/backend/config/jwt/JwtAuthenticationFilter.java
@@ -33,13 +33,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
 
                 //유효한 토큰인지 확인
-                if (token != null && jwtTokenProvider.validateToken(request, token)) {
+                if (token != null && jwtTokenProvider.validateToken(token)) {
                     // 토큰이 유효하면 토큰에서 유저 정보를 받아온다.
-                    Authentication authentication = jwtTokenProvider.getAuthentication(request, token);
-                    // SecurityContext에 Authentication 객체를 저장
+                    Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                    // SecurityContext에 Authentication 객체를 저장 (getAuthentication()시 jwt에 따라 값을 가져온다)
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
-
         }
         filterChain.doFilter(request, response);
     }
