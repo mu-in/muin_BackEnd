85406b76054a21127ddfbdc202e0d4bdebc5aafe
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-10-27 14:12:21 +0900

    feat : 매장 리스트 조회 구현 [#32]
    - 하버사인 공식으로 구형(지구) 위에서 두 지점 사이 거리 구함

--- src/main/java/dev/muin/backend/domain/Store/StoreRepository.java
+++ src/main/java/dev/muin/backend/domain/Store/StoreRepository.java
@@ -1,9 +1,23 @@
 package dev.muin.backend.domain.Store;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Short> {
+
+    String HAVERSINE = "(6371 * acos(cos(radians(:userLat)) * cos(radians(s.location.latitude)) * cos(radians(s.location.longitude) - radians(:userLon)) + sin(radians(:userLat)) * sin(radians(s.location.latitude))))";
+
+    // JPQL SELECT 구문 빨간줄 오류 아님
+    @Query("SELECT s, " + HAVERSINE +
+            "FROM Store s " +
+            "WHERE " + HAVERSINE + "<= :distance " +
+            "ORDER BY" + HAVERSINE)
+    List<Object[]> findAllByDistanceASC(@Param("userLat") double userLat, @Param("userLon") double userLon,
+                                        @Param("distance") double distance);
+
     Optional<Store> findByUuid(String uuid);
 }
