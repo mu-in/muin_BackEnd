842523b536355f782163aefefbdc45f002eacc7a
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-10-28 00:34:13 +0900

    db : 스프링부트 애플리케이션 시작시 상품 data 로드 [#30]
    - Product.java: 자동 키생성 전략 삭제
    - data.sql: 상품 데이터 로드
    - Products.csv: 상품 데이터 csv 추가
    
    Resolved #30

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
+LOAD DATA LOCAL INFILE './src/main/resources/csvData/Products.csv' INTO TABLE product
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES
+(@category_large,@category_medium,@category_small,@class,@data_id,@name,@price) set product_id=@data_id,name=@name, price=@price, category=@category_large;
+
 insert into store(store_id, uuid, name, latitude, longitude) value(1,'aaaaaaaaa-aaaa-aaaa-aaaaaaaaaaaa', '민주어린이집_500m',37.54475295439056, 127.06857657373725);
 insert into store(store_id, uuid, name, latitude, longitude) value(2,'baaaaaaaa-aaaa-aaaa-aaaaaaaaaaaa', '쭈꾸미킹_500m',37.543356243294255, 127.07018177829258);
 insert into store(store_id, uuid, name, latitude, longitude) value(3,'caaaaaaaa-aaaa-aaaa-aaaaaaaaaaaa', '화양초등학교_500m',37.54434516425474, 127.07102601237384);
