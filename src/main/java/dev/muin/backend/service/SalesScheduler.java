0d1c83fe97680b21ef9b3ba0389427b2cf1b6424
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-11-13 23:50:45 +0900

    style : 매장 월매출 스케쥴링 수정
    - 미사용 메서드 삭제
    - 로그 추가

--- src/main/java/dev/muin/backend/service/SalesScheduler.java
+++ src/main/java/dev/muin/backend/service/SalesScheduler.java
@@ -25,23 +25,14 @@ public class SalesScheduler {
     @Scheduled(cron="0 0 0 1 * *") //매월 1일 0시
     public void getMonthlySales(){
         List<Store> stores = storeRepository.findAll();
-        log.info("Store is start to get monthly sales....");
+        log.info("Store is start to get monthly sales.......");
         for(Store s: stores) {
             Integer monthlySales = paymentRepository.findThisMonthByStore(s.getId());
             if(monthlySales == null) monthlySales = 0;
             Sales sales = new Sales(LocalDate.now(), monthlySales, s);
             salesRepository.save(sales);
+            log.info(String.format("store(id=%d,name=%s)'s sales=%d", s.getId(), s.getName(), monthlySales));
         }
-    }
-
-    @Scheduled(cron="30 * * * * *") //매 정시
-    public void test(){
-        List<Store> stores = storeRepository.findAll();
-        log.info("[TEST] Store is start to get hourly sales....");
-        for(Store s: stores) {
-            Integer hourlySales = paymentRepository.findThisMonthByStore(s.getId());
-            if(hourlySales==null) hourlySales = 0;
-            log.info(String.format("store(id=%d,name=%s)'s sales=%d", s.getId(), s.getName(), hourlySales));
-        }
+        log.info("End to get monthly sales.......");
     }
 }
